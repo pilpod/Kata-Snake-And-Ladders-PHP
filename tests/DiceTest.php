b06f271ce6c21035099387529faa06072dcabcd8
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2022-10-01 14:18:44 +0200

    DiceTest refactored

--- tests/DiceTest.php
+++ tests/DiceTest.php
@@ -7,20 +7,18 @@ use PHPUnit\Framework\TestCase;
 
 class DiceTest extends TestCase {
 
-    private Dice $dice1;
-    private Dice $dice2;
+    private Dice $dice;
 
     protected function setUp() : void
     {
-        $this->dice1 = new Dice;
-        $this->dice2 = new Dice;
+        $this->dice = new Dice;
     }
 
     /** @test */
     public function test_dice_value_1_to_6()
     {
-        $this->dice1->roll();
-        $result = $this->dice1->getScore();
+        $this->dice->roll();
+        $result = $this->dice->getScore();
         
         $this->assertIsInt($result);
         $this->assertGreaterThanOrEqual(1, $result);
